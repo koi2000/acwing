5e927f3b53b27c167515945538f5e106949c8ac6
koi2000 <1754520660@qq.com>
2023-01-18 10:10:52 +0800

    新增第四周每日一题

--- winter/week04/day02/main.cpp
+++ winter/week04/day02/main.cpp
@@ -1,9 +1,123 @@
+#pragma GCC optimize(3)
+#pragma GCC optimize(2)
 #include "bits/stdc++.h"
-
 #define ll long long
 using namespace std;
 
+
 int main() {
-    ios::sync_with_stdio(false);
+//    ios::sync_with_stdio(false);
+    int n, l, s;
+//    cin >> n >> l >> s;
+    scanf("%d %d %d", &n, &l, &s);
+    set<pair<int, int>> tree;
+    for (int i = 0; i < n; ++i) {
+        int x, y;
+//        cin >> x >> y;
+        scanf("%d %d", &x, &y);
+        tree.insert({x, y});
+    }
+    int map[s + 1][s + 1];
+    int tp;
+    for (int i = s; i >= 0; --i) {
+        for (int j = 0; j <= s; ++j) {
+            scanf("%d", &tp);
+            map[i][j] = tp;
+        }
+    }
+    int res = 0;
+    for (auto tr:tree) {
+        int flag = 1;
+        for (int i = 0; i <= s; ++i) {
+            for (int j = 0; j <= s; ++j) {
+                int x = tr.first + i;
+                int y = tr.second + j;
+                if (x > l || y > l || tree.count({x, y}) != map[i][j]) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                break;
+            }
+        }
+        if (flag)res++;
+    }
+    cout << res << endl;
     return 0;
 }
+
+// Created by csp on 22-6-12.
+//
+//#include <bits/stdc++.h>
+//
+//#define ll long long
+//#define lowbit(x) (x & -x)
+//using namespace std;
+//const int maxn = 1e6 + 10;
+//
+//unordered_map<int, set<int>> mapx;
+//unordered_map<int, set<int>> mapy;
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cout.tie(nullptr);
+//    int n, L, s;
+//    cin >> n >> L >> s;
+//    int x, y;
+//    int mat[s + 1][s + 1];
+//    for (int i = 0; i < n; ++i) {
+//        cin >> x >> y;
+//        mapx[x].insert(y);
+//        mapy[y].insert(x);
+//    }
+//    int maps[s + 1][s + 1];
+//    for (int i = s; i >= 0; --i) {
+//        for (int j = 0; j <= s; ++j) {
+//            cin >> maps[i][j];
+//        }
+//    }
+//    int res = 0;
+//    for (auto &item: mapx) {
+//        set<int> col = item.second;
+//        for (auto i: col) {
+//            int flag = 1;
+//            for (int j = 0; j < s + 1; ++j) {
+//                for (int k = 0; k < s + 1; ++k) {
+//                    int xx = item.first + j;
+//                    int yy = i + k;
+//                    if (xx > L || yy > L) {
+//                        flag = 0;
+//                        break;
+//                    }
+//                    if (maps[j][k]) {
+//                        if (mapx.count(xx)) {
+//                            if (mapx[xx].count(yy)) {
+//                                continue;
+//                            }
+//                        }
+//                    } else {
+//                        if (!mapx.count(xx)) {
+//                            continue;
+//                        } else {
+//                            if (!mapx[xx].count(yy)) {
+//                                continue;
+//                            }
+//                        }
+//                    }
+//                    flag = 0;
+//                    break;
+//                }
+//                if (flag == 0) {
+//                    break;
+//                }
+//            }
+//            if (flag) {
+//                res++;
+//            }
+//        }
+//    }
+//    cout << res << endl;
+//    return 0;
+//}
--- winter/week04/day03/main.cpp
+++ winter/week04/day03/main.cpp
@@ -2,8 +2,70 @@
 
 #define ll long long
 using namespace std;
+const int N = 1e5 + 10;
+struct brNode {
+    brNode *l;
+    brNode *r;
+    int val;
+};
+vector<int> nodes[N];
+int n;
+
+int dfs(int node) {
+    int count = 0;
+    if (nodes[node].size() == 0)return 0;
+    for (int i = 0; i < nodes[node].size(); ++i) {
+        count = max(count, dfs(nodes[node][i]));
+    }
+    return count + nodes[node].size();
+}
+
+void build(brNode *root) {
+    if (!root) {
+        return;
+    }
+    int node = root->val;
+    if (nodes[node].size() == 0) {
+        return;
+    }
+    root->l = new brNode();
+    root->l->val = nodes[node][0];
+    brNode *head = new brNode();
+    brNode *r = new brNode();
+    head->r = r;
+    for (int i = 1; i < nodes[node].size(); ++i) {
+        r->val = nodes[node][i];
+        r->l = nullptr;
+        if (i != nodes[node].size() - 1) {
+            r->r = new brNode();
+            r = r->r;
+        } else {
+            r->r = nullptr;
+        }
+
+    }
+    if (nodes[node].size() > 1) {
+        root->r = head->r;
+    } else {
+        root->r = nullptr;
+    }
+    build(root->l);
+    build(root->r);
+}
+
 
 int main() {
     ios::sync_with_stdio(false);
+
+    cin >> n;
+    for (int i = 0; i < n - 1; ++i) {
+        int fa;
+        cin >> fa;
+        nodes[fa].push_back(i + 2);
+    }
+    cout << dfs(1);
+    brNode *root = new brNode();
+    root->val = 1;
+    build(root);
     return 0;
 }
