8fd433c7f1b53a8e827800ef0dbe71ad1d18fff6
koi2000 <1754520660@qq.com>
2023-01-26 09:27:02 +0800

    每日一题完结

--- winter/week04/day04/main.cpp
+++ winter/week04/day04/main.cpp
@@ -3,7 +3,29 @@
 #define ll long long
 using namespace std;
 
+int qpow(int a, int b) {
+
+    int res = 1;
+    while (b) {
+        if (b & 1) {
+            res = res * a;
+            if (res > 1e9) {
+                return -1;
+            }
+        }
+        if (a >= sqrt(1e9) && b >= 2) {
+            return -1;
+        }
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
 int main() {
     ios::sync_with_stdio(false);
+    int a, b;
+    cin >> a >> b;
+    cout << qpow(a, b);
     return 0;
 }
--- winter/week04/day05/main.cpp
+++ winter/week04/day05/main.cpp
@@ -5,5 +5,26 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n, a, b;
+        cin >> n >> a >> b;
+        ll m = n - a * b + 2;
+        ll l = 1, r = m >> 1;
+        while (l < r) {
+            ll mid = l + r >> 1;
+            if (mid * (m - mid) >= n) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        if (l * (m - l) == n) {
+            cout << l << " " << m - l << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
     return 0;
 }
