a56dfede9d95a8d636509450ce5c79bf98bca375
koi2000 <1754520660@qq.com>
2023-03-17 09:51:42 +0800

    新增博弈论

--- bridge/week05/day05/main.cpp
+++ bridge/week05/day05/main.cpp
@@ -4,7 +4,27 @@
 #define pii pair<int,int>
 using namespace std;
 
+int n, k;
+
 int main() {
     ios::sync_with_stdio(false);
+    int T;
+    cin >> T;
+    while (T--) {
+        cin >> n >> k;
+        if (k % 3 == 0) {
+            n %= (k + 1);
+            if (n == k) {
+                cout << "Alice" << endl;
+                continue;
+            }
+        }
+        if (n % 3 == 0) {
+            cout << "Bob" << endl;
+        } else {
+            cout << "Alice" << endl;
+        }
+
+    }
     return 0;
 }
